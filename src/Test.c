a26aae99dc9be43ebe2357b8b85a2438819e9c9b
Your Name <you@example.com>
2021-09-08 23:34:29 -0700

    test commit

--- src/Test.c
+++ src/Test.c
@@ -1,15 +1,26 @@
-/*
- * Test.c
- *
- *  Created on: Sep 8, 2021
- *      Author: meld0047
- */
-
+/****************************************************************************
+ File name:	HelloWorld.c
+ Author:     chadd williams
+ Date:       7/19/18
+ Class:      CS300
+ Assignment: C Examples
+ Purpose:    Simple C Demo
+ ****************************************************************************/
 
 #include <stdio.h>
 
+/****************************************************************************
+ Function:			main
+
+ Description:	Display hello world
+
+ Parameters:		None
+
+ Returned:			0 for Success
+ ****************************************************************************/
 int main ()
 {
-	printf( "Hello World" );
+	printf ("Hello World! \nI'm Elijah");
+	printf ( " This it a Test!")
 	return 0;
 }
